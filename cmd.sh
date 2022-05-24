7c6591b5d8cba1ac7d1c02eabd38786e22c58a30
qiaofeng1227 <76487013@qq.com>
2022-05-24 15:02:52 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # customrized cmd powered by Websoft9
 
-rm -f /www/server/panel/data/admin_path.pl 
-echo $BAOTA_USER | bt 6  
-echo $BAOTA_PASSWORD | bt 5 
-/aapanel.sh || /bt.sh
+
+
+if [[ -f /opt/bitnami/scripts/libmysqlclient.sh ]]; then
+    rm -f /www/server/panel/data/admin_path.pl 
+    echo $BAOTA_USER | bt 6  
+    echo $BAOTA_PASSWORD | bt 5 
+    /aapanel.sh || /bt.sh
+else
+    echo "Initialization has been completed before this time!"
+fi
