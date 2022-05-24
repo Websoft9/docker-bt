a99535e04385e8cdd889ada684033539bae9c9b8
qiaofeng1227 <76487013@qq.com>
2022-05-24 16:49:22 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -5,7 +5,7 @@ if [[ -f /www/server/panel/data/admin_path.pl ]]; then
     rm -f /www/server/panel/data/admin_path.pl 
     echo $BAOTA_USER | bt 6  
     echo $BAOTA_PASSWORD | bt 5 
-    /aapanel.sh || /bt.sh
 else
     echo "Initialization has been completed before this time!"
 fi
+/aapanel.sh || /bt.sh
